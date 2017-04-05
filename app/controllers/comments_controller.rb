b0d89958249bcd53ca809f42d753a77537f5d2b7
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-04-04 22:43:29 -0700

    Add errors to posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     post = Post.find(params[:post_id])
     comment = post.comments.new(body: comment_params[:body], user_id: current_user.id)
@@ -7,7 +9,6 @@ class CommentsController < ApplicationController
       ## js
       redirect_to post_path(post)
     else
-      flash[:notice] = 'Please fill in the comment field.'
       redirect_to post_path(post)
     end
   end
